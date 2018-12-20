f55cfbaaedb1b6252ba049c9294adda9abbc34a4
Diego Lopes <dihgg@dihgg.com>
2018-12-20 19:03:21 -0200

    organização dos arquivos PHP

--- wp-content/themes/gamefoss/functions.php
+++ wp-content/themes/gamefoss/functions.php
@@ -9,75 +9,28 @@ sidebars, comments, etc.
 */
 
 // LOAD BONES CORE (if you remove this, the theme will break)
-require_once( 'library/bones.php' );
-// CUSTOMIZE THE WORDPRESS ADMIN (off by default)
-// require_once( 'library/admin.php' );
+require_once( 'theme/bones.php' );
 
-/*********************
-LAUNCH BONES
-Let's get everything up and running.
-*********************/
-
-function bones_ahoy() {
-
-	//Allow editor style.
-	add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
-
-	// let's get language support going, if you need it
-	load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
-
-	// USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
-	require_once( 'custom_posts/custom-post-type.php' );
-
-	// launching operation cleanup
-	add_action( 'init', 'bones_head_cleanup' );
-	// A better title
-	add_filter( 'wp_title', 'rw_title', 10, 3 );
-	// remove WP version from RSS
-	add_filter( 'the_generator', 'bones_rss_version' );
-	// remove pesky injected css for recent comments widget
-	add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
-	// clean up comment styles in the head
-	add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
-	// clean up gallery output in wp
-	add_filter( 'gallery_style', 'bones_gallery_style' );
-
-	// enqueue base scripts and styles
-	add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
-	// ie conditional wrapper
-
-	// launching this stuff after theme setup
-	bones_theme_support();
-
-	// cleaning up random code around images
-	add_filter( 'the_content', 'bones_filter_ptags_on_images' );
-
-} /* end bones ahoy */
-
-// let's get this party started
-add_action( 'after_setup_theme', 'bones_ahoy' );
+// LOAD THEME CORE
+require_once('theme/theme.php');
 
 
-/************* OEMBED SIZE OPTIONS *************/
-
-if ( ! isset( $content_width ) ) {
-	$content_width = 680;
-}
+require_once( 'custom_posts/custom-post-type.php' );
 
 /************************************
 CUSTOM FUNCTIONS
 ************************************/
-require_once('custom-functions.php');
+require_once('functions/custom-functions.php');
 
 /************************************
 CUSTOM HOOKS
 ************************************/
-require_once('custom-hooks.php');
+require_once('functions/custom-hooks.php');
 
 /************************************
 CRONS
 ************************************/
-require_once('cron.php');
+require_once('functions/cron.php');
 
 /* DON'T DELETE THIS CLOSING TAG */
 ?>
