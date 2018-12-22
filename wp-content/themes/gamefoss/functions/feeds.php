2b51eb5f257c7d35e7e4ea1a8507a4ca2946d13c
Diego Lopes <dihgg@dihgg.com>
2018-12-22 13:53:11 -0200

    podcast feed tweaks

--- wp-content/themes/gamefoss/functions/feeds.php
+++ wp-content/themes/gamefoss/functions/feeds.php
@@ -13,17 +13,14 @@
 
 	// CUSTOM FEEDS
 	// Custom feed for each house podcast
-	function podcast_feed($_podcast) {
-		header( 'Content-Type: application/rss+xml' );
-		set_query_var('podcast', $GLOBALS['podcast']);
-		get_template_part( 'rss/rss', 'podcast' );
-		unset( $GLOBALS['podcast'] );
-	}
 	add_action('init', function () {
 		foreach ( get_terms( "podcasts", array( 'hide_empty' => false) ) as $_podcast ) {
-			$GLOBALS['podcast'] = $_podcast;
 			if ( get_field( "external", $_podcast ) ) continue;
-			add_feed("podcasts/{$_podcast->slug}", 'podcast_feed');
+			add_feed("podcasts/{$_podcast->slug}", function() use ( $_podcast ) {
+				header( 'Content-Type: application/rss+xml' );
+				set_query_var('podcast', $_podcast);
+				get_template_part( 'rss/rss', 'podcast' );
+			});
 		}
 	});
 
