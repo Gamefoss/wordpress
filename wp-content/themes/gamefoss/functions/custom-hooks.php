4d07c5361b06130def22556168df7c43cd3a53ae
Diego Lopes <dihgg@dihgg.com>
2019-01-04 03:17:56 -0200

    Ajustes

--- wp-content/themes/gamefoss/functions/custom-hooks.php
+++ wp-content/themes/gamefoss/functions/custom-hooks.php
@@ -1,2 +1,6 @@
 <?php
+	// ADD LUDOKRATIA POSTS TO AUTHOR PAGE
+	add_action( 'pre_get_posts', function ( $query ) {
+		if ( !is_admin() && $query->is_author() && $query->is_main_query() ) $query->set( 'post_type', array('post', 'ludokratia' ) );
+	} );
 ?>
