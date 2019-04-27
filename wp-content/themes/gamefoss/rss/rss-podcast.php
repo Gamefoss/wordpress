1e5b0674cd4d50841c17e87c9570140fd467a956
Diego Lopes <dihgg@dihgg.com>
2019-04-26 23:35:14 -0300

    ajustes no feed do podcast para o spotify

--- wp-content/themes/gamefoss/rss/rss-podcast.php
+++ wp-content/themes/gamefoss/rss/rss-podcast.php
@@ -5,33 +5,22 @@
 	xmlns:atom="http://www.w3.org/2005/Atom"
 	xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
 	xmlns:itunesu="http://www.itunesu.com/feed"
+	xmlns:spotify="https://www.spotify.com/ns/rss"
 	version="2.0"
 >
 	<channel>
-		<language>
-			pt-br
-		</language>
-		<title>
-			<?php echo $podcast->name ?>
-		</title>
-		<description>
-			<?php echo $podcast->description ?>
-		</description>
+		<title><?php echo $podcast->name ?></title>
 		<link><?php echo get_term_link($podcast); ?></link>
-
+		<description><?php echo $podcast->description ?></description>
+		<language>pt-br</language>
+		
 		<?php if ( get_field("is_itunes", $podcast) ): ?>
-			<itunes:subtitle>
-				<?php echo get_field("itunes", $podcast)["description"] ?>
-			</itunes:subtitle>
-			<itunes:author>
-				<?php echo get_field("itunes", $podcast)["author"] ?>
-			</itunes:author>
-			<itunes:summary>
-				<?php echo $podcast->description ?>
-			</itunes:summary>
+			<itunes:author><?php echo get_field("itunes", $podcast)["author"] ?></itunes:author>
+			<itunes:subtitle><?php echo get_field("itunes", $podcast)["description"] ?></itunes:subtitle>
+			<itunes:summary><?php echo $podcast->description ?></itunes:summary>
 			<itunes:owner>
-			    <itunes:name>Game Foss</itunes:name>
-			    <itunes:email>admin@gamefoss.com</itunes:email>
+				<itunes:name>Game Foss</itunes:name>
+				<itunes:email>admin@gamefoss.com</itunes:email>
 			</itunes:owner>
 			<itunes:explicit>no</itunes:explicit>
 			<itunes:image href="<?php echo get_field("logo", $podcast) ?>" />
@@ -48,7 +37,7 @@
 			'numberposts'		=> -1,
 			'tax_query'			=> array(
 				array(
-					'taxonomy'	=> 'podcasts',
+					'taxonomy'		=> 'podcasts',
 					'field'			=> 'id',
 					'terms'			=> $podcast->term_id
 				)
@@ -56,33 +45,19 @@
 		) ) as $post ): ?>
 			<?php setup_postdata( $post ) ?>
 			<item>
-			    <title>
-					<?php echo get_the_title() ?>
-				</title>
-				<description>
-					<![CDATA[<?php echo get_the_content() ?>]]>
-				</description>
-				<itunes:subtitle>
-					<![CDATA[<?php echo get_field("itunes", $podcast)["description"] ?>]]>
-				</itunes:subtitle>
-			    <itunes:summary>
-			    	<![CDATA[<?php echo get_the_content() ?>]]>
-			    </itunes:summary>
-			    <description>
-			    	<?php echo get_the_excerpt() ?>
-			    </description>
-			    <link><?php echo get_the_permalink() ?></link>
-			    <enclosure url="<?php echo get_field( "mp3" )["url"] ?>" type="audio/mpeg" length="<?php echo get_field( "mp3" )["filesize"] ?>"></enclosure>
+				<guid><?php echo get_the_guid() ?></guid>
+				<title><![CDATA[<?php echo get_the_title() ?>]]></title>
+				<description><![CDATA[<?php echo get_the_content() ?>]]></description>
+				<pubDate><?php echo get_the_date( "r" ) ?></pubDate>
+				<link><?php echo get_the_permalink() ?></link>
+				<enclosure url="<?php echo get_field( "mp3" )["url"] ?>" type="audio/mpeg" length="<?php echo get_field( "mp3" )["filesize"] ?>"></enclosure>
 				<?php if ( get_field("is_itunes", $podcast) ): ?>
+					<itunes:subtitle><![CDATA[<?php echo get_field("itunes", $podcast)["description"] ?>]]></itunes:subtitle>
+					<itunes:summary><![CDATA[<?php echo get_the_content() ?>]]></itunes:summary>
 					<itunes:author>GameFoss</itunes:author>
-				    <itunes:duration>
-				    	<?php echo get_field( "itunes" )["duration"] ?>
-				    </itunes:duration>
-				    <itunes:explicit>no</itunes:explicit>
+					<itunes:duration><?php echo get_field( "itunes" )["duration"] ?></itunes:duration>
+					<itunes:explicit>no</itunes:explicit>
 				<?php endif ?>
-				<pubdate>
-					<![CDATA[<?php echo get_the_date( "r" ) ?>]]>
-				</pubdate>
 			</item>
 			<?php wp_reset_postdata() ?>
 		<?php endforeach ?>
