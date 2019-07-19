ef9043fae7f13cf8eccb5d59da65b565a7b8c9b6
Diego Lopes <dihgg@dihgg.com>
2019-07-19 18:08:24 -0300

    #18 - Ajustar og tags

--- wp-config.php
+++ wp-config.php
@@ -1,24 +1,6 @@
 <?php
-/**
- * As configurações básicas do WordPress
- *
- * O script de criação wp-config.php usa esse arquivo durante a instalação.
- * Você não precisa usar o site, você pode copiar este arquivo
- * para "wp-config.php" e preencher os valores.
- *
- * Este arquivo contém as seguintes configurações:
- *
- * * Configurações do MySQL
- * * Chaves secretas
- * * Prefixo do banco de dados
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/pt-br:Editando_wp-config.php
- *
- * @package WordPress
- */
 
-// Environment configuration
+// ENVIRONMENT
 switch ($_SERVER['SERVER_NAME']) {
 	case "gamefoss.com":
 	case "gamefoss.com.br":
@@ -29,10 +11,10 @@ switch ($_SERVER['SERVER_NAME']) {
 	break;
 }
 
-// Protocol configuration
+// PROTOCOL CONFIGURATION
 define('WP_PROTOCOL', $_SERVER['HTTPS']? "https://" : "http://");
 
-// ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
+// DATABASE CONFIGURATION
 switch (WP_ENV) {
 	case "localhost":
 	define('DB_NAME', 'gamefoss');
@@ -47,26 +29,14 @@ switch (WP_ENV) {
 	define('DB_HOST', 'localhost');
 	break;
 }
-
-/** Charset do banco de dados a ser usado na criação das tabelas. */
 define('DB_CHARSET', 'utf8mb4');
-
-/** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
 define('DB_COLLATE', '');
+$table_prefix  = 'wp_';
 
-/**#@+
- * Chaves únicas de autenticação e salts.
- *
- * Altere cada chave para um frase única!
- * Você pode gerá-las
- * usando o {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org
- * secret-key service}
- * Você pode alterá-las a qualquer momento para invalidar quaisquer
- * cookies existentes. Isto irá forçar todos os
- * usuários a fazerem login novamente.
- *
- * @since 2.6.0
- */
+/*	SALTS CONFIGURATION
+*	https://api.wordpress.org/secret-key/1.1/salt/
+*/
+
 define('AUTH_KEY',         'KJTW47]G^1h9AALjYW/yjEJ[^j^{.zj?*1k.|uo,us6}}IB=Mw~D+7oq/kgEXx)4');
 define('SECURE_AUTH_KEY',  'EX~]]Od` zS#V$=SPiCP6XxRpao~DZAY?zag_.0CIjg]jAhq$qN`dO;!:}/-LmL2');
 define('LOGGED_IN_KEY',    '`_KQeZN.x&6c`$U?j >Pq}zI{`ObK13*bi|c7tf`-)qduxm1/<{IS_W#PJ/u/~.,');
@@ -76,31 +46,7 @@ define('SECURE_AUTH_SALT', 's21wUrrUWxTHt_<bY81RrB@@nt}Xa!`@bdOighVM8_cV3cQ?TAqG
 define('LOGGED_IN_SALT',   '3p@IZbuNF(`zp*T@hy=eC:v(cSX K!XTj;] ,$x]a7z.}UX(#xA&`nF@A=hCgyUL');
 define('NONCE_SALT',       'eiYwgkgY#K3xCz<~li h-sae,Lml1w{FZp4[Tna2hQD2H2 2&HLeK1%^U?&qen,Y');
 
-/**#@-*/
-
-/**
- * Prefixo da tabela do banco de dados do WordPress.
- *
- * Você pode ter várias instalações em um único banco de dados se você der
- * um prefixo único para cada um. Somente números, letras e sublinhados!
- */
-$table_prefix  = 'wp_';
-
-/**
- * Para desenvolvedores: Modo de debug do WordPress.
- *
- * Altere isto para true para ativar a exibição de avisos
- * durante o desenvolvimento. É altamente recomendável que os
- * desenvolvedores de plugins e temas usem o WP_DEBUG
- * em seus ambientes de desenvolvimento.
- *
- * Para informações sobre outras constantes que podem ser utilizadas
- * para depuração, visite o Codex.
- *
- * @link https://codex.wordpress.org/pt-br:Depura%C3%A7%C3%A3o_no_WordPress
- */
-
-/* more changes of env */
+// ENVIRONMENT CONFIGURATION
 switch (WP_ENV) {
 	case "localhost":
 		define('WP_SITEURL', WP_PROTOCOL . $_SERVER['SERVER_NAME'] ."/gamefoss");
@@ -110,23 +56,15 @@ switch (WP_ENV) {
 	case "development":
 		break;
 	case "production":
-		define('WP_SITEURL', "https://" . $_SERVER['SERVER_NAME']);
-		define('WP_HOME',    "https://" . $_SERVER['SERVER_NAME']);
 		define('WP_DEBUG', false);
-		// Force SSL for administrator and sign in pages
+		// Force SSL
+		define('WP_HOME',    "https://" . $_SERVER['SERVER_NAME']);
+		define('WP_SITEURL', "https://" . $_SERVER['SERVER_NAME']);
 		define('FORCE_SSL_ADMIN', true);
 		break;
 }
 
-// define('CONCATENATE_SCRIPTS', false);
+// WP STUFF
+if ( !defined('ABSPATH') ) define('ABSPATH', dirname(__FILE__) . '/');
 
-// desabilitar o cron padrão do WP
-// define('DISABLE_WP_CRON', true);
-/* Isto é tudo, pode parar de editar! :) */
-
-/** Caminho absoluto para o diretório WordPress. */
-if ( !defined('ABSPATH') )
-define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Configura as variáveis e arquivos do WordPress. */
 require_once(ABSPATH . 'wp-settings.php');
